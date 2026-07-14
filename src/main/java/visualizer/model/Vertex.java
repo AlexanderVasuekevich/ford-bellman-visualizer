46cf8937037056d16ba5083ef17758580e56789c
MorderrLink <burmenskiy90@bk.ru>
2026-07-15 00:07:23 +0300

    Добавлен парсер графа из файла

--- src/main/java/visualizer/model/Vertex.java
+++ src/main/java/visualizer/model/Vertex.java
@@ -1,5 +1,8 @@
 package visualizer.model;
 
+/**
+ * Одна вершина графа с именем и координатами для отображения.
+ */
 public class Vertex {
     private String name;
     private int x;
@@ -11,22 +14,47 @@ public class Vertex {
         this.y = y;
     }
 
+    /**
+     * Возвращает уникальное имя вершины.
+     *
+     * @return имя вершины
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Возвращает координату X на холсте.
+     *
+     * @return координата X
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Устанавливает координату X на холсте.
+     *
+     * @param x координата X
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Возвращает координату Y на холсте.
+     *
+     * @return координата Y
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Устанавливает координату Y на холсте.
+     *
+     * @param y координата Y
+     */
     public void setY(int y) {
         this.y = y;
     }
